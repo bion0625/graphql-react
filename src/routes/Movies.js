f71fcb204937e8b816951f3b81398aa90b8ab655
이의정 <stxtory@gmail.com>
2023-09-23 13:04:08 +0900

    useParams

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,4 +1,5 @@
 import { gql, useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
 
 const GET_MOVIES = gql`
     query getMovies{
@@ -24,16 +25,17 @@ const Movies = () => {
     if(error){
         return <h1>Could not fetch :(</h1>
     }
-    return (<ul>
-            <h1>Movies</h1>
-            {data.allMovies.map(movie => (
-                <li key={movie.id}>{movie.title}</li>
-            ))}
-            <h1>Tweets</h1>
-            {data.allTweets.map(tweet => (
-                <li key={tweet.id}>{tweet.text}/by{tweet.author.fullName}</li>
-            ))}
-        </ul>);
+    return (
+        <ul>
+            {data.allMovies.map(movie => 
+                <li key={movie.id}>
+                    <Link to={`/movies/${movie.id}`}>
+                        {movie.title}
+                    </Link>
+                </li>
+            )}
+        </ul>
+    );
 }
 
 export default Movies;
